41786d656c89f7de9dc4f29763a1db90f77715e3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-02 19:17:37 -0400

    working server

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
 var path = require('path');
 
@@ -5,3 +7,25 @@ var content = fs.readFileSync('README.md');
 var start = fs.readFileSync(path.join(__dirname, 'template/head.html'));
 var end = fs.readFileSync(path.join(__dirname, 'template/body.html'));
 
+var page = start + '\n' + content + '\n' + end;
+
+var http = require('http');
+var port = 3700;
+
+function hostPage(page) {
+	var server = http.createServer(function (req, res) {
+		console.log(req.url);
+		if (req.url === '/') {
+			res.end(page);
+		} else {
+			var full = path.join(__dirname, 'lib', req.url);
+			res.end(fs.readFileSync(full));
+		}
+	});
+
+	server.listen(port, function () {
+		console.log('listening at port', port);
+	});
+}
+
+hostPage(page);
