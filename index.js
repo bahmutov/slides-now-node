27d5d2978db777b6d46648d23dc62b36cc3b1947
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-02 19:26:35 -0400

    filename to serve read from command line

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 #!/usr/bin/env node
 
+if (process.argv.length < 3) {
+	console.error('missing .md filename');
+	process.exit(-1);
+}
+
+var mdFilename = process.argv[2];
+var mdName = /\.md|\.markdown|\.txt|\.md.txt$/i;
+if (!mdName.test(mdFilename)) {
+	console.error('invalid markdown filename', mdFilename);
+	console.error('should end with .md');
+	process.exit(-2);
+}
+
 var fs = require('fs');
 var path = require('path');
+var open = require('opener')
+var http = require('http');
 
-var content = fs.readFileSync('README.md');
+var content = fs.readFileSync(mdFilename);
 var start = fs.readFileSync(path.join(__dirname, 'template/head.html'));
 var end = fs.readFileSync(path.join(__dirname, 'template/body.html'));
 
 var page = start + '\n' + content + '\n' + end;
 
-var http = require('http');
 var port = 3700;
 
 function hostPage(page) {
@@ -20,11 +34,19 @@ function hostPage(page) {
 		} else {
 			var full = path.join(__dirname, 'lib', req.url);
 			res.end(fs.readFileSync(full));
+
+			if (req.url === '/slides-now.js') {
+				console.log('served application, exitting in 2 seconds');
+				setTimeout(function () {
+					process.exit(0);
+				}, 2000);
+			}
 		}
 	});
 
 	server.listen(port, function () {
 		console.log('listening at port', port);
+		open('http://localhost:' + port);
 	});
 }
 
